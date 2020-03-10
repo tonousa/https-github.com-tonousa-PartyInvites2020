461892c2dec23bc3ddb677161ea53c26928e9a87
MS24912 <MS24912@HDQITSW529541>
2020-03-10 17:11:51 -0500

    Ch 3 AutoProperty

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Navigate to a URL";
+        }
+
+        public ViewResult AutoProperty()
+        {
+            Product myProduct = new Product();
+
+            myProduct.Name = "Kayak";
+
+            string productName = myProduct.Name;
+
+            return View("Result",
+                (object)String.Format("Product name: {0}", productName));
+        }
+    }
+}
