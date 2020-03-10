c229fdcb78dc024cc5ad52cd6067a91caddd3139
MS24912 <MS24912@HDQITSW529541>
2020-03-10 17:22:50 -0500

    Automatic Properties, no getter, setter bodies or fields

--- LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/Models/Product.cs
@@ -7,12 +7,10 @@ namespace LanguageFeatures.Models
 {
     public class Product
     {
-        private string name;
-
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
     }
 }
