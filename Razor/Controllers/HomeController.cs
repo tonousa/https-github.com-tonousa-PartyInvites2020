0a681cac54c52504ad006a2421ed8512640fd963
MS24912 <MS24912@HDQITSW529541>
2020-03-13 15:13:22 -0500

    Ch 6

--- Razor/Controllers/HomeController.cs
+++ Razor/Controllers/HomeController.cs
@@ -38,6 +38,20 @@ namespace Razor.Controllers
 
             return View(myProd);
         }
+
+        public ActionResult DemoArray()
+        {
+            Product[] array =
+            {
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "Lifejacket", Price = 48.95M},
+                new Product {Name = "Soccer ball", Price = 19.50M},
+                new Product {Name = "Corner flag", Price = 34.95M}
+            };
+
+            return View(array);
+        }
+
     }
 
 }
