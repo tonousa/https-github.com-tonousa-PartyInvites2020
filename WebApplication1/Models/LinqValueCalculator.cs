6919b8c8a26d249e7ff575c32285c78b5363a360
tonousa <tonousa@hotmail.com>
2020-03-15 20:10:39 -0500

    Register Dependency Resolver

---
+++ WebApplication1/Models/LinqValueCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class LinqValueCalculator : IValueCalculator
+    {
+        public decimal ValueProducts(IEnumerable<Product> products)
+        {
+            return products.Sum(p => p.Price);
+        }
+    }
+}
