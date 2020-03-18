10e8c7085d69ffc9eb36fc4c90e12a4ecfe862f5
MS24912 <MS24912@HDQITSW529541>
2020-03-18 12:12:39 -0500

    Unit Test: [ExpectedException(typeof...] attribute

--- WebApp1.Tests/UnitTest1.cs
+++ WebApp1.Tests/UnitTest1.cs
@@ -25,5 +25,49 @@ namespace WebApp1.Tests
             // assert
             Assert.AreEqual(total * 0.9M, discountedTotal);
         }
+
+        [TestMethod]
+        public void Discount_Bet_10_And_100()
+        {
+            // Arrange
+            IDiscountHelper target = getTestObject();
+            decimal total = 200;
+
+            // act
+            decimal TenDollarDiscount = target.ApplyDiscount(10);
+            decimal HundredDollarDiscount = target.ApplyDiscount(100);
+            decimal FiftyDollarDiscount = target.ApplyDiscount(50);
+
+            // assert
+            Assert.AreEqual(5, TenDollarDiscount, "$10 discount is wrong");
+            Assert.AreEqual(95, HundredDollarDiscount, "$100 discount is wrong");
+            Assert.AreEqual(45, FiftyDollarDiscount, "$50 discount is wrong");
+        }
+
+        [TestMethod]
+        public void Discount_Below_10()
+        {
+            // Arrange
+            IDiscountHelper target = getTestObject();
+
+            // act
+            decimal discount5 = target.ApplyDiscount(5);
+            decimal discount0 = target.ApplyDiscount(0);
+
+            // assert
+            Assert.AreEqual(5, discount5);
+            Assert.AreEqual(0, discount0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Discount_Negative_Total()
+        {
+            // Arrange
+            IDiscountHelper target = getTestObject();
+
+            // Act
+            target.ApplyDiscount(-1);
+        }
     }
 }
